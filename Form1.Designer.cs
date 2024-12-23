fb76f6484cfeb78ae1ab225292333fed2e202c08
DewaPermana22 <dewarisa77@gmail.com>
2024-12-24 01:55:41 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace LKS_JATIM_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxPeriod = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.currencyConverterDataSet = new LKS_JATIM_2024.CurrencyConverterDataSet();
+            this.periodBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.periodTableAdapter = new LKS_JATIM_2024.CurrencyConverterDataSetTableAdapters.PeriodTableAdapter();
+            this.currencyConverterDataSet1 = new LKS_JATIM_2024.CurrencyConverterDataSet1();
+            this.currencyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.currencyTableAdapter = new LKS_JATIM_2024.CurrencyConverterDataSet1TableAdapters.CurrencyTableAdapter();
+            this.ConvertBinding = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyConverterDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.periodBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyConverterDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConvertBinding)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(411, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Currency Converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(201, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Period";
+            // 
+            // comboBoxPeriod
+            // 
+            this.comboBoxPeriod.DataSource = this.periodBindingSource;
+            this.comboBoxPeriod.DisplayMember = "name";
+            this.comboBoxPeriod.FormattingEnabled = true;
+            this.comboBoxPeriod.ItemHeight = 13;
+            this.comboBoxPeriod.Location = new System.Drawing.Point(262, 121);
+            this.comboBoxPeriod.Name = "comboBoxPeriod";
+            this.comboBoxPeriod.Size = new System.Drawing.Size(521, 21);
+            this.comboBoxPeriod.TabIndex = 2;
+            this.comboBoxPeriod.ValueMember = "id";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(93, 215);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 200);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Origin Amunt :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(663, 215);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(348, 200);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Converted To :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(506, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 97);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.currencyBindingSource;
+            this.comboBox1.DisplayMember = "abbreviation";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(230, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.ValueMember = "id";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label4";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(27, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(237, 112);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(93, 28);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // currencyConverterDataSet
+            // 
+            this.currencyConverterDataSet.DataSetName = "CurrencyConverterDataSet";
+            this.currencyConverterDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // periodBindingSource
+            // 
+            this.periodBindingSource.DataMember = "Period";
+            this.periodBindingSource.DataSource = this.currencyConverterDataSet;
+            // 
+            // periodTableAdapter
+            // 
+            this.periodTableAdapter.ClearBeforeFill = true;
+            // 
+            // currencyConverterDataSet1
+            // 
+            this.currencyConverterDataSet1.DataSetName = "CurrencyConverterDataSet1";
+            this.currencyConverterDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // currencyBindingSource
+            // 
+            this.currencyBindingSource.DataMember = "Currency";
+            this.currencyBindingSource.DataSource = this.currencyConverterDataSet1;
+            // 
+            // currencyTableAdapter
+            // 
+            this.currencyTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBoxPeriod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyConverterDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.periodBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyConverterDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConvertBinding)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.ComboBox comboBoxPeriod;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private CurrencyConverterDataSet currencyConverterDataSet;
+        private System.Windows.Forms.BindingSource periodBindingSource;
+        private CurrencyConverterDataSetTableAdapters.PeriodTableAdapter periodTableAdapter;
+        private CurrencyConverterDataSet1 currencyConverterDataSet1;
+        private System.Windows.Forms.BindingSource currencyBindingSource;
+        private CurrencyConverterDataSet1TableAdapters.CurrencyTableAdapter currencyTableAdapter;
+        private System.Windows.Forms.BindingSource ConvertBinding;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LKS_JATIM_2024
+{
+    public partial class Form1 : Form
+    {
+        CurrencyConverterEntities ccEntity = new CurrencyConverterEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currencyBindingSource.DataSource = ccEntity.Currency.ToList();
+            periodBindingSource.DataSource = ccEntity.Period.ToList();
+            ConvertBinding.DataSource = ccEntity.Currency.ToList();
+
+            comboBox2.DisplayMember = "abbreviation";
+            comboBox2.ValueMember = "id";
+            comboBox2.DataSource = ConvertBinding.DataSource;
+
+            comboBox1.SelectedValue = 0;
+            comboBox2.SelectedValue = 0;
+            comboBoxPeriod.SelectedValue = 0;
+            label3.Text = string.Empty;
+            label4.Text = string.Empty;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idSelect = Convert.ToInt32(comboBox1.SelectedValue);
+            var nama_mata_uang = ccEntity.Currency.FirstOrDefault(cc => cc.id == idSelect);
+            label3.Text = nama_mata_uang?.name ?? "Invalid Name Currency";
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idSelect = Convert.ToInt32(comboBox2.SelectedValue);
+            var nama_mata_uang = ccEntity.Currency.FirstOrDefault(cc => cc.id == idSelect);
+            label4.Text = nama_mata_uang?.name ?? "Invalid Name Currency";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var asal_mata_uang = comboBox1.SelectedValue;
+            comboBox1.SelectedValue = comboBox2.SelectedValue;
+            comboBox2.SelectedValue = asal_mata_uang;
+        }
+
+        // 1. Kita Ambil dulu Value dari masing masing combo box
+        private decimal ambil_nilai(decimal amount) {
+            if (comboBox1.SelectedValue == null || comboBox1.SelectedValue == null) return 0;
+            {
+                var asal_mata_uang = Convert.ToInt32(comboBox1.SelectedValue);
+                var tujuan_mata_uang = Convert.ToInt32(comboBox2.SelectedValue);
+                var period = Convert.ToInt32(comboBoxPeriod.SelectedValue);
+                if (asal_mata_uang == tujuan_mata_uang)
+                {
+                    MessageBox.Show("Abbreviation Must be Different!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                if (period == 0)
+                {
+                    MessageBox.Show("Select Period first!", "Warnig!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                decimal rate = kalkulasi_kurs(asal_mata_uang, tujuan_mata_uang, period);
+
+                return amount * rate;
+            }
+        }
+
+        // 2. Buat fungsi Untuk menghitung kurs mata uang ke dolar
+        private decimal kalkulasi_kurs(int mata_uang_asal, int mata_uang_tujuan, int period){
+            var kurs_mata_uang_asal = ccEntity.USDExchangeRate.Where(i => i.currency_id == mata_uang_asal && i.period_id == period).FirstOrDefault();
+            var kurs_mata_uang_tujuan = ccEntity.USDExchangeRate.Where(i => i.currency_id == mata_uang_tujuan && i.period_id == period).FirstOrDefault();
+            if (kurs_mata_uang_asal == null || kurs_mata_uang_tujuan == null)
+            {
+                return 0;
+            }
+            var hasil_kurs = kurs_mata_uang_asal.rate / kurs_mata_uang_tujuan.rate;
+            return hasil_kurs;
+        }
+
+        // 3. ambil inputan pengguna di textbox
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(textBox1.Text, out decimal input))
+            {
+                decimal hasil_akhir = ambil_nilai(input);
+                textBox2.Text = hasil_akhir.ToString("N2");
+            }
+            else
+            {
+                MessageBox.Show("Input Invalid!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Clear();
+            }
+        }
+
+
+    }
+}
